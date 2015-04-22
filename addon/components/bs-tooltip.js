f89e5c8f994f1b2cb1a55715cb3353a4485ce563
simon <ihmig@kaliber5.de>
2015-04-22 21:33:23 +0200

    merged i18n support from master

--- addon/components/bs-tooltip.js
+++ addon/components/bs-tooltip.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 import TooltipMixin from 'ember-bootstrap/mixins/tooltip-support';
+import I18nSupport from 'ember-bootstrap/mixins/i18n-support';
 
-export default Ember.Component.extend(TooltipMixin, {
+export default Ember.Component.extend(TooltipMixin, I18nSupport, {
     tagName: 'span'
 });
